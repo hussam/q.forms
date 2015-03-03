e8166c30e56a21ac6a69c4dc0f21514361ae41b7
Hussam Abu-Libdeh <hussam@0xff.co>
2015-03-02 21:18:04 -0500

    Modified label on inspiration cell

--- cravery/Views/InspirationCell.cs
+++ cravery/Views/InspirationCell.cs
@@ -14,11 +14,12 @@ namespace cravery
 			image.SetBinding (Image.SourceProperty, "Picture");
 
 			var description = new Label {
-				//FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
+				FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
 				FontAttributes = FontAttributes.Bold,
 				YAlign = TextAlignment.Center,
-				XAlign = TextAlignment.End,
-				BackgroundColor = Color.White.MultiplyAlpha(0.65)
+				XAlign = TextAlignment.Center,
+				LineBreakMode = LineBreakMode.TailTruncation,
+				BackgroundColor = Color.White.MultiplyAlpha(0.6)
 			};
 			description.SetBinding (Label.TextProperty, "Name");
 
@@ -26,7 +27,7 @@ namespace cravery
 			AbsoluteLayout.SetLayoutFlags (image, AbsoluteLayoutFlags.SizeProportional);
 			AbsoluteLayout.SetLayoutBounds (image, new Rectangle (0, 0, 1, 1));
 			AbsoluteLayout.SetLayoutFlags (description, AbsoluteLayoutFlags.All);
-			AbsoluteLayout.SetLayoutBounds (description, new Rectangle (0, 0.5, 0.25, 0.4));
+			AbsoluteLayout.SetLayoutBounds (description, new Rectangle (0, 0.75, 1, 0.25));
 
 			layout.Children.Add (image);
 			layout.Children.Add (description);
